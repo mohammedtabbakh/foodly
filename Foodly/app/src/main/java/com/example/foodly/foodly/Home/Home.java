393fd36b3d0968f8614ece23a62d127bfab0a980
Raghad_Kaadan <rory-_-04@hotmail.com>
2019-05-04 18:16:01 +0300

    show login layout first

--- Foodly/app/src/main/java/com/example/foodly/foodly/Home/Home.java
+++ Foodly/app/src/main/java/com/example/foodly/foodly/Home/Home.java
@@ -3,8 +3,6 @@ package com.example.foodly.foodly.Home;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
-
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +13,8 @@ import com.example.foodly.foodly.R;
 public class Home extends Fragment {
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
         return inflater.inflate(R.layout.home, container, false);
     }
 
@@ -24,6 +22,7 @@ public class Home extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("الرئيسية");
     }
