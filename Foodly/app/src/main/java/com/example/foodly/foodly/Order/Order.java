857a439c73dc2eb2e96f922387397f2738d48a04
Mohammed Tabbakh <mt16555@gmail.com>
2019-05-14 20:21:51 +0300

    Add CartItem and connect it to order UI

--- Foodly/app/src/main/java/com/example/foodly/foodly/Order/Order.java
+++ Foodly/app/src/main/java/com/example/foodly/foodly/Order/Order.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 import com.example.foodly.foodly.R;
@@ -11,7 +13,16 @@ import com.example.foodly.foodly.R;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
+import Models.CartItem;
+
 public class Order extends Fragment {
+
+    ListView itemsListView;
+    CartItemsAdapter  itemsAdapter;
+    ArrayList<CartItem> items;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,5 +36,14 @@ public class Order extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("الطلبات");
+        items = new ArrayList<CartItem>();
+        items.add(new CartItem(){{setItemName("فول"); }});
+        items.add(new CartItem(){{setItemName("حمص"); }});
+        items.add(new CartItem(){{setItemName("تفاح"); }});
+        items.add(new CartItem(){{setItemName("برتقال"); }});
+
+        itemsListView = getView().findViewById(R.id.items_list_view);
+        itemsAdapter = new CartItemsAdapter(getContext(),items);
+        itemsListView.setAdapter(itemsAdapter);
     }
 }
