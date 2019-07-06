b8bfce2f29c66220d0cf9489a692a45ed04b5280
Nizar <nizar.nasani@gmail.com>
2019-07-06 15:28:05 +0300

    added order dialog

--- Foodly/app/src/main/java/com/example/foodly/foodly/Order/Order.java
+++ Foodly/app/src/main/java/com/example/foodly/foodly/Order/Order.java
@@ -1,16 +1,20 @@
 package com.example.foodly.foodly.Order;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 import com.example.foodly.foodly.R;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
@@ -28,14 +32,44 @@ public class Order extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.order, container, false);
+       View view=inflater.inflate(R.layout.order, container, false);
+       buyingButton=view.findViewById(R.id.buyBtn);
+
+      /* buyingButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+
+               alertDialogBuilder.setTitle("تأكيد الشراء");
+
+               alertDialogBuilder
+                       .setMessage("هل تريد تأكيد عملية الشراء")
+                       .setCancelable(false)
+                       .setPositiveButton("نعم",new DialogInterface.OnClickListener() {
+                           public void onClick(DialogInterface dialog,int id) {
+                             dialog.dismiss();
+                               Toast toast = Toast.makeText(getActivity(), "صحتين و هنا سلفاً D:", Toast.LENGTH_SHORT);
+                               toast.setGravity(Gravity.CENTER, 0, 0);
+                               toast.show();
+                           }
+                       })
+                       .setNegativeButton("لا",new DialogInterface.OnClickListener() {
+                           public void onClick(DialogInterface dialog, int id) {
+                               dialog.cancel();
+                           }
+                       });
+               AlertDialog alertDialog = alertDialogBuilder.create();
+               alertDialog.show();
+           }
+       });*/
+       return view;
+
     }
 
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        buyingButton = getView().findViewById(R.id.addButton);
 
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("الطلبات");
@@ -60,6 +94,29 @@ public class Order extends Fragment {
             @Override
             public void onClick(View v) {
                 onBuyingButtonClicked();
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+
+                        alertDialogBuilder.setTitle("تأكيد الشراء");
+
+                        alertDialogBuilder
+                                .setMessage("هل تريد تأكيد عملية الشراء")
+                                .setCancelable(false)
+                                .setPositiveButton("نعم",new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.dismiss();
+                                        Toast toast = Toast.makeText(getActivity(), "صحتين و هنا سلفاً D:", Toast.LENGTH_SHORT);
+
+                                        toast.show();
+                                    }
+                                })
+                                .setNegativeButton("لا",new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        AlertDialog alertDialog = alertDialogBuilder.create();
+                        alertDialog.show();
+
             }
         });
     }
