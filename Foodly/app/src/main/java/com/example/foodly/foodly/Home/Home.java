527bb19b4e19675ae6f22b0390ddffcbc4437bbd
Nizar <nizar.nasani@gmail.com>
2019-07-07 09:54:29 +0300

    home recycler gets data from api

--- Foodly/app/src/main/java/com/example/foodly/foodly/Home/Home.java
+++ Foodly/app/src/main/java/com/example/foodly/foodly/Home/Home.java
@@ -11,6 +11,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.foodly.foodly.Api.ApiClient;
+import com.example.foodly.foodly.Api.ApiEndPoints;
+import com.example.foodly.foodly.Meal.MealName;
 import com.example.foodly.foodly.Meal.adapter.RecyclerAdapter;
 import com.example.foodly.foodly.R;
 
@@ -18,6 +21,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Models.Meal;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class Home extends Fragment {
@@ -27,13 +33,17 @@ public class Home extends Fragment {
     private Meal mealData;
     private List<Meal> SuggestHomeList;
     private List<Meal> MostDemandList;
+    List<Models.Meal> MealsList;
+    int i;
+
+
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-       View view = inflater.inflate(R.layout.home, container, false);
+      final View view = inflater.inflate(R.layout.home, container, false);
 
        SuggestHomeRecyclerView=view.findViewById(R.id.suggested_home_recycler);
        MostDemandRecyclerView=view.findViewById(R.id.mostDemandRecycler);
@@ -46,62 +56,91 @@ public class Home extends Fragment {
         GridLayoutManager GridLayoutManager1 = new GridLayoutManager(view.getContext(), 3);
         SuggestHomeRecyclerView.setLayoutManager(GridLayoutManager1);
 
+        ApiEndPoints apiEndPoints = ApiClient.getClient().create(ApiEndPoints.class);
+
+        MealsList = new ArrayList<>();
 
 
-       MostDemandList=new ArrayList<>();
-
-        mealData = new Meal("يبرق");
-        MostDemandList.add(mealData);
-
-        mealData = new Meal("فريكة");
-        MostDemandList.add(mealData);
-
-        mealData = new Meal("يبرق");
-        MostDemandList.add(mealData);
-
-        mealData = new Meal("يبرق");
-        MostDemandList.add(mealData);
-
-        mealData = new Meal("يبرق");
-        MostDemandList.add(mealData);
-
-        mealData = new Meal("يبرق");
-        MostDemandList.add(mealData);
-
-        mealData = new Meal("يبرق");
-        MostDemandList.add(mealData);
-
-
-
-
-
-        SuggestHomeList=new ArrayList<>();
-
-        mealData = new Meal("فاصولية بيضاء");
-        SuggestHomeList.add(mealData);
-
-        mealData = new Meal("فريكة");
-        SuggestHomeList.add(mealData);
-
-        mealData = new Meal("فاصولية بيضاء");
-        SuggestHomeList.add(mealData);
-
-        mealData = new Meal("فاصولية بيضاء");
-        SuggestHomeList.add(mealData);
-
-        mealData = new Meal("فاصولية بيضاء");
-        SuggestHomeList.add(mealData);
-
-        mealData = new Meal("فاصولية بيضاء");
-        SuggestHomeList.add(mealData);
-
-        RecyclerAdapter recyclerAdapter1 = new RecyclerAdapter(view.getContext(),MostDemandList);
-        MostDemandRecyclerView.setAdapter(recyclerAdapter1);
+//
+//       MostDemandList=new ArrayList<>();
+//
+//        mealData = new Meal("يبرق");
+//        MostDemandList.add(mealData);
+//
+//        mealData = new Meal("فريكة");
+//        MostDemandList.add(mealData);
+//
+//        mealData = new Meal("يبرق");
+//        MostDemandList.add(mealData);
+//
+//        mealData = new Meal("يبرق");
+//        MostDemandList.add(mealData);
+//
+//        mealData = new Meal("يبرق");
+//        MostDemandList.add(mealData);
+//
+//        mealData = new Meal("يبرق");
+//        MostDemandList.add(mealData);
+//
+//        mealData = new Meal("يبرق");
+//        MostDemandList.add(mealData);
+//
+//
+//
+//
+//
+//        SuggestHomeList=new ArrayList<>();
+//
+//        mealData = new Meal("فاصولية بيضاء");
+//        SuggestHomeList.add(mealData);
+//
+//        mealData = new Meal("فريكة");
+//        SuggestHomeList.add(mealData);
+//
+//        mealData = new Meal("فاصولية بيضاء");
+//        SuggestHomeList.add(mealData);
+//
+//        mealData = new Meal("فاصولية بيضاء");
+//        SuggestHomeList.add(mealData);
+//
+//        mealData = new Meal("فاصولية بيضاء");
+//        SuggestHomeList.add(mealData);
+//
+//        mealData = new Meal("فاصولية بيضاء");
+//        SuggestHomeList.add(mealData);
 
 
-        RecyclerAdapter recyclerAdapter2 = new RecyclerAdapter(view.getContext(),SuggestHomeList);
-        SuggestHomeRecyclerView.setAdapter(recyclerAdapter2);
-        SuggestHomeRecyclerView.setHasFixedSize(true);
+        apiEndPoints.GetAllMeals().enqueue(new Callback<List<Meal>>() {
+            @Override
+            public void onResponse(Call<List<Meal>> call, Response<List<Meal>> response) {
+
+                MealsList = response.body();
+
+                for( i=0;i<=MealsList.size()/2;i++)
+                {
+                    MostDemandList=new ArrayList<>();
+                    MostDemandList.add(MealsList.get(i));
+
+                }
+                RecyclerAdapter recyclerAdapter1 = new RecyclerAdapter(view.getContext(),MostDemandList);
+                MostDemandRecyclerView.setAdapter(recyclerAdapter1);
+
+                for( i=MealsList.size()/2;i<MealsList.size();i++)
+                {
+                    SuggestHomeList=new ArrayList<>();
+                   SuggestHomeList.add(MealsList.get(i));
+                }
+
+                RecyclerAdapter recyclerAdapter2 = new RecyclerAdapter(view.getContext(),SuggestHomeList);
+                SuggestHomeRecyclerView.setAdapter(recyclerAdapter2);
+            }
+
+            @Override
+            public void onFailure(Call<List<Meal>> call, Throwable t) {
+                MealsList.add(new Meal("فريكة"));
+                MealsList.add(new Meal("فاصوليا"));
+            }
+        });
 
 
        return view;
