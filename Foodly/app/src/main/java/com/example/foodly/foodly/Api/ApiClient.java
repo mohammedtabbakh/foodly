ad90fa77ce6714361c1d74099e1d14c7330133ca
Mohammed Tabbakh <mt16555@gmail.com>
2019-07-07 08:56:13 +0300

    Change base url

--- Foodly/app/src/main/java/com/example/foodly/foodly/Api/ApiClient.java
+++ Foodly/app/src/main/java/com/example/foodly/foodly/Api/ApiClient.java
@@ -11,7 +11,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL="http://192.168.1.105:45455/";
+    public static final String BASE_URL="http://mohammedtabbakh-001-site1.gtempurl.com";
     private static Retrofit retrofit = null;
 
 
